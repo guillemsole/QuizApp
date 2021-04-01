4fd8fe1230971e40792417cad09db2aa71736e88
guillemsole <guillemsolecubilo@gmail.com>
2021-04-01 17:28:43 +0200

    ADD QuestionViewController with testing

---
+++ QuizApp/QuestionViewController.swift
@@ -0,0 +1,43 @@
+//
+//  QuestionViewController.swift
+//  QuizApp
+//
+//  Created by Guillem Solé Cubilo on 1/4/21.
+//
+
+import Foundation
+import UIKit
+
+class QuestionViewController: UIViewController {
+    
+    @IBOutlet weak var headerLabel: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    
+    private var question: String = ""
+    private var options: [String] = []
+
+    convenience init(question: String, options: [String]) {
+        self.init()
+        self.question = question
+        self.options = options
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        headerLabel.text = question
+    }
+}
+
+extension QuestionViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return options.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = options[indexPath.row]
+        
+        return cell
+    }
+}
