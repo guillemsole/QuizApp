c191cae9c77fd17036081e2ea287e345a980fae6
guillemsole <guillemsolecubilo@gmail.com>
2021-04-02 19:09:57 +0200

    ADD NavigationControllerRouter concrete implementation

---
+++ QuizApp/NavigationControllerRouter.swift
@@ -0,0 +1,22 @@
+//
+//  Created by Guillem Solé Cubilo on 2/4/21.
+//
+
+import UIKit
+import QuizEngine
+
+class NavigationControllerRouter: Router {
+    private let navigationController: UINavigationController
+    
+    init(_ navigationController: UINavigationController) {
+        self.navigationController = navigationController
+    }
+    
+    func routeTo(question: String, answerCallback: @escaping (String) -> Void) {
+        navigationController.pushViewController(UIViewController(), animated: false)
+    }
+    
+    func routeTo(result: Result<String, String>) {
+        
+    }
+}
