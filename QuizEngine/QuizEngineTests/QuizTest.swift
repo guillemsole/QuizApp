f722236bf8b7cbce2ef8c1d4f65321fa4ef71eb0
guillemsole <guillemsolecubilo@gmail.com>
2021-04-27 16:00:28 +0200

    Deprecated the deprecated test. it will be the startposition for the new API

--- QuizEngine/QuizEngineTests/QuizTest.swift
+++ QuizEngine/QuizEngineTests/QuizTest.swift
@@ -3,3 +3,56 @@
 //
 
 import Foundation
+import XCTest
+import QuizEngine // We don't make it teastable it means we are testing the public interface and as an integration test this is what we want
+// Integration test
+@available(*, deprecated)
+class QuizTest: XCTestCase {
+    
+    private let router = RouterSpy()
+    private var game: Game<String, String, RouterSpy>!
+    
+    override func setUp() {
+        super.setUp()
+        game = startGame(questions: ["Q1","Q2"], router: router, correctAnswers: ["Q1": "A1", "Q2": "A2"])
+
+    }
+    func test_startGame_answerZeroOutOfTwoCorrectly_scoresZero() {
+        router.answerCallback("wrong")
+        router.answerCallback("wrong")
+        
+        XCTAssertEqual(router.routedResult!.score, 0)
+    }
+    
+    func test_startGame_answerOneOutOfTwoCorrectly_scoresOne() {
+        router.answerCallback("A1")
+        router.answerCallback("wrong")
+        
+        XCTAssertEqual(router.routedResult!.score, 1)
+    }
+    
+    
+    func test_startGame_answerTwoOutOfTwoCorrectly_scoresTwo() {
+        router.answerCallback("A1")
+        router.answerCallback("A2")
+        
+        XCTAssertEqual(router.routedResult!.score, 2)
+    }
+    
+    class RouterSpy: Router {
+        var routedResult: Result<String, String>? = nil
+        var routedQuestions: [String] = []
+        
+        var answerCallback: (String) -> Void = { _ in }
+        
+        func routeTo(question: String, answerCallback: @escaping (String) -> Void) {
+            routedQuestions.append(question)
+            self.answerCallback = answerCallback
+        }
+        
+        func routeTo(result: Result<String, String>) {
+            routedResult = result
+        }
+    }
+}
+
