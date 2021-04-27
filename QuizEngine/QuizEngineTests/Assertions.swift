0c216d1177926fd4f95ddc3be419432c570e7157
guillemsole <guillemsolecubilo@gmail.com>
2021-04-27 19:54:19 +0200

    Remove duplication

---
+++ QuizEngine/QuizEngineTests/Assertions.swift
@@ -0,0 +1,10 @@
+//
+//  Created by Guillem Solé Cubilo on 27/4/21.
+//
+
+import Foundation
+import XCTest
+
+func assertEqual(_ a1: [(String, String)], _ a2: [(String, String)], file: StaticString = #file, line: UInt = #line) {
+    XCTAssertTrue(a1.elementsEqual(a2, by: ==), "\(a1) is not equal to \(a2)", file: file, line: line)
+}
