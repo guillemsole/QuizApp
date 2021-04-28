cb7167880c832b2e1e8ca7c4ddca7e54f1f84420
guillemsole <guillemsolecubilo@gmail.com>
2021-04-28 15:16:52 +0200

    Extract single selection cell subview

--- QuizApp/UI/SwiftUI/SingleAnswerQuestion.swift
+++ QuizApp/UI/SwiftUI/SingleAnswerQuestion.swift
@@ -17,17 +17,7 @@ struct SingleAnswerQuestion: View {
         VStack(alignment: .leading, spacing: 0.0) {
             QuestionHeader(title: title, question: question)
             ForEach(options, id:\.self) { option in
-                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
-                    HStack {
-                        Circle()
-                            .stroke(Color.secondary, lineWidth: 2.5)
-                            .frame(width: 40.0, height: 40.0)
-                        Text(option)
-                            .font(.title)
-                            .foregroundColor(Color.secondary)
-                        Spacer()
-                    }.padding()
-                })
+                SingleTextSelectionCell(text: option, selection: {})
             }
             Spacer()
         }
@@ -62,4 +52,6 @@ struct SingleAnswerQuestion_Previews: PreviewProvider {
         }
     }
 }
+
+
 
