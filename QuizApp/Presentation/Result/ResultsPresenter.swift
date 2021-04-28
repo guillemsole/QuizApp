d77c7bf8be821bb9c4a8d87eebd21294393fcc97
guillemsole <guillemsolecubilo@gmail.com>
2021-04-28 11:07:30 +0200

    Removed the ResultsPresenter deprecate initializer

--- QuizApp/Presentation/Result/ResultsPresenter.swift
+++ QuizApp/Presentation/Result/ResultsPresenter.swift
@@ -22,15 +22,6 @@ final class ResultsPresenter {
         self.scorer = scorer
     }
     
-    init(result: Result<Question<String>, [String]>, questions: [Question<String>], correctAnswers: [Question<String>: [String]]) {
-        self.userAnswers = questions.map { question in
-            (question, result.answers[question]!)
-        }
-        self.correctAnswers = questions.map { question in
-            (question, correctAnswers[question]!)
-        }
-        self.scorer = { _, _ in result.score}
-    }
     var summary: String {
         return "You got \(score)/\(userAnswers.count) correct"
     }
