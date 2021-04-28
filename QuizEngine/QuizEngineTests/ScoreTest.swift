8b588d4c33e1d7fcd523658ce2a8ecd0a33dddfd
guillemsole <guillemsolecubilo@gmail.com>
2021-04-28 08:42:36 +0200

    Score correct answer along with wrong answer and we can score more than one correct answer

--- QuizEngine/QuizEngineTests/ScoreTest.swift
+++ QuizEngine/QuizEngineTests/ScoreTest.swift
@@ -15,14 +15,27 @@ class ScoreTest: XCTestCase {
         XCTAssertEqual(BasicScore.score(for: ["wrong"], comparingTo: ["correct"]), 0)
     }
     
-    func test_oneCorrectAnswer_scoresZero() {
-        XCTAssertEqual(BasicScore.score(for: ["correct"], comparingTo: ["correct"]), 0)
+    func test_oneCorrectAnswer_scoresOne() {
+        XCTAssertEqual(BasicScore.score(for: ["correct"], comparingTo: ["correct"]), 1)
+    }
+    
+    func test_oneCorrectAnswerOneWrong_scoresOne() {
+        let score = BasicScore.score(for: ["correct 1", "wrong"], comparingTo: ["correct 1", "correct 2"])
+        XCTAssertEqual(score, 1)
+    }
+    
+    func test_twoCorrectAnswers_scoresTwo() {
+        let score = BasicScore.score(for: ["correct 1", "correct 2"], comparingTo: ["correct 1", "correct 2"])
+        XCTAssertEqual(score, 2)
     }
     
     private class BasicScore {
         static func score(for answers: [String], comparingTo correctAnswers: [String]) -> Int {
-            if answers.isEmpty { return 0 }
-            return answers == correctAnswers ? 1: 0
+            var score = 0
+            for (index, answer) in answers.enumerated() {
+                score += (correctAnswers[index] == answer) ? 1 : 0
+            }
+            return score
         }
     }
 }
