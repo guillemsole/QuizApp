5787c6af51edbd800aeb8efcf85505ae0870dbd4
guillemsole <guillemsolecubilo@gmail.com>
2021-04-28 11:14:32 +0200

    Introduce the new Factory initializer

--- QuizApp/Main/iOSViewControllerFactory.swift
+++ QuizApp/Main/iOSViewControllerFactory.swift
@@ -6,10 +6,18 @@ import UIKit
 import QuizEngine
 
 class iOSViewControllerFactory: ViewControllerFactory {
+    typealias Answers = [(question: Question<String>, answers: [String])]
     private let questions: [Question<String>]
     private let options: [Question<String>: [String]]
-    private let correctAnswers: [Question<String>: [String]]
+    private let correctAnswers: () -> Answers
 
+    
+    init(options: [Question<String>: [String]], correctAnswers: Answers) {
+        self.questions = correctAnswers.map { $0.question }
+        self.options = options
+        self.correctAnswers = { correctAnswers }
+    }
+    
     init(
         questions: [Question<String>],
         options: [Question<String>: [String]],
@@ -17,9 +25,12 @@ class iOSViewControllerFactory: ViewControllerFactory {
     ) {
         self.questions = questions
         self.options = options
-        self.correctAnswers = correctAnswers
+        self.correctAnswers = { questions.map { question in
+                (question, correctAnswers[question]!)
+            }
+        }
     }
-
+    
     func questionViewController(for question: Question<String>, answerCallback: @escaping ([String]) -> Void) -> UIViewController {
         guard let options = options[question] else {
             fatalError("Couldn't find options for question: \(question)")
@@ -48,9 +59,7 @@ class iOSViewControllerFactory: ViewControllerFactory {
     func resultViewController(for result: Result<Question<String>, [String]>) -> UIViewController {
         let presenter = ResultsPresenter(userAnswers: questions.map { question in
             (question, result.answers[question]!)
-        }, correctAnswers: questions.map { question in
-            (question, correctAnswers[question]!)
-        }, scorer: { _, _ in result.score})
+        }, correctAnswers: correctAnswers(), scorer: { _, _ in result.score})
         return ResultViewController(summary: presenter.summary, answers: presenter.presentableAnswers)
     }
 
